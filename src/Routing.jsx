65651aefbcd28e117af386dc411d187315561121
Hlovegood <hlovegood14@gmail.com>
2026-03-28 20:07:38 +0200

    Routing Base

---
+++ src/Routing.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop';
+
+const Routing = () => {
+    return ( <>
+    <BrowserRouter>
+        <ScrollToTop/>
+        <Routes>
+            
+        </Routes>
+    
+    
+    
+    
+    
+    </BrowserRouter>
+    
+    
+    </> );
+}
+ 
+export default Routing;
