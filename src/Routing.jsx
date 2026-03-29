1848e21550b1d59f4cf69c1a127680961cfe9968
Hlovegood <hlovegood14@gmail.com>
2026-03-29 09:40:18 +0200

    Nav Links

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop';
+import Home from './pages/Home'
 
 const Routing = () => {
     return ( <>
     <BrowserRouter>
         <ScrollToTop/>
         <Routes>
-            
+            <Route path='/' element={<Home/>}/>
         </Routes>
     
     
---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.css'
+
+const Nav = () => {
+    return ( <>
+    <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to ="/AR-Page">AR</Link></li>
+        <li><Link to ="/Contact">Contact</Link></li>
+        <li><Link to="/Reviews-Page">Community</Link></li>
+        <li><Link to="/Feed-Page">Feed</Link></li>
+        <li><Link to ="/Careers-Page">Careers</Link></li>
+        <li><Link to ="/Profile-Page">Profile</Link></li>
+    </ul>
+    
+    </> );
+}
+ 
+export default Nav;
