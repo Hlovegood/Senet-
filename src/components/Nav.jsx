22841e626849147fa748f762912bbeffe9b19bb6
Hlovegood <hlovegood14@gmail.com>
2026-03-29 12:10:28 +0200

    Nav Styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css'
+import Lang from '../assets/Icons/Lang-Icon.png'
+import Profile from '../assets/Icons/Profile-Icon.png'
+import Senet from '../assets/Icons/Senet-Icon.png'
+
 
 const Nav = () => {
     return ( <>
     <nav>
 
     <ul className='Nav-Cont'>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to ="/AR-Page">AR</Link></li>
-        <li><Link to ="/Contact">Contact</Link></li>
-        <li><Link to="/Reviews-Page">Community</Link></li>
-        <li><Link to="/Feed-Page">Feed</Link></li>
-        <li><Link to ="/Careers-Page">Careers</Link></li>
-        <li><Link to ="/Profile-Page">Profile</Link></li>
+        <li className='Nav-link'><Link to="/"><img src={Senet} alt="" /></Link></li>
+        <li className='Nav-link'><Link to ="/AR-Page">AR</Link></li>
+        <li className='Nav-link'><Link to ="/Contact">Contact</Link></li>
+        <li className='Nav-link'><Link to="/Reviews-Page">Community</Link></li>
+        <li className='Nav-link'><Link to="/Feed-Page">Feed</Link></li>
+        <li className='Nav-link'><Link to ="/Careers-Page">Careers</Link></li>
+        <li className='Nav-link'><Link to ="/Profile-Page"><img src={Profile} alt="Profile" /></Link></li>
     </ul>
     </nav>
     
